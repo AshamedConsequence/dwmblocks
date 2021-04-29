11f44c0e4d4eaf181fd52f4ab2a3c6c232f8d4e9
Maxime Morize <morizem@awty.org>
2021-04-28 21:35:46 -0500

    Configured stuff

--- config.h
+++ config.h
@@ -3,9 +3,9 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	/* {"⌨", "kbselect", 0, 30}, */
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	{"",	"music",	0,	11},
-	{"",	"pacpackages",	0,	8},
-	{"",	"news",		0,	6},
+	{"",	"sb-music",	0,	11},
+	{"",	"sb-pacpackages",	0,	8},
+	{"",	"sb-news",		0,	6},
 	/* {"",	"crypto",	0,	13}, */
 	/* {"",	"price lbc \"LBRY Token\" 📚",			9000,	22}, */
 	/* {"",	"price bat \"Basic Attention Token\" 🦁",	9000,	20}, */
@@ -17,14 +17,14 @@ static const Block blocks[] = {
 	/* {"",	"memory",	10,	14}, */
 	/* {"",	"cpu",		10,	18}, */
 	/* {"",	"moonphase",	18000,	17}, */
-	{"",	"forecast",	18000,	5},
+	{"",	"sb-forecast",	18000,	5},
 	/* {"",	"mailbox",	180,	12}, */
 	/* {"",	"nettraf",	1,	16}, */
-	{"",	"volume",	0,	10},
-	{"",	"battery",	5,	3},
-	{"",	"clock",	1,	1},
-	{"",	"internet",	5,	4},
-	{"",	"help-icon",	0,	15},
+	{"",	"sb-volume",	0,	10},
+	/* {"",	"sb-battery",	5,	3}, */
+	{"",	"sb-clock",	1,	1},
+	{"",	"sb-internet",	5,	4},
+	{"",	"sb-help-icon",	0,	15},
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
@@ -34,3 +34,4 @@ static char *delim = " | ";
 // vim with the following line in your vimrc/init.vim:
 
 // autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
+
